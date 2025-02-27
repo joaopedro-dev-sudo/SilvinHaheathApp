d09c862d47106e766d34e7282d76942206d0344b
joaopedro-dev-sudo <pedrosantoaraujo0908@gmail.com>
2025-02-27 12:26:26 -0300

    estiliza

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <view style={styles.titlebox}>
-        <text style={StyleSheet.TITLETEXT}>Silvinha Health APP</text>
-      </view>
-    
-      <StatusBar STYLE="auto" /> 
-    <View>  
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleBox}>
+        <Text style={styles.title}>Silvinha Health APP</Text>
+      </View>
+
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#edf2f4',
   },
+tittleBox: {
+alignItems: 'center',
+justifyContent: 'flex-end',
+height: 100,
+backgroundColor: '#ef233c',
+borderBottomStartRadius: 25,
+borderBottomEndRadius: 25,
+},
+titleText: {
+  color: '#edf2f4',
+  fontsize: 28,
+  fontweight: 'bold',
+  marginbottom: 20,
+  }  
 });
